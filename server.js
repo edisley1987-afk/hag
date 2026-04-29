bea10f3ecd1b38d65e4ab2bef34516b896140475
edisley1987-afk <edisley1987@gmail.com>
2026-04-29 13:43:35 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -414,7 +414,8 @@ if (tsAtual < tsAnterior) {
     } else if (sensor.capacidade) {
       const valorAtual = Number(rawVal) || 0;
       const anterior = Number(novo[ref]) || valorAtual;
-      
+      const suavizado = (anterior * 0.8) + (valorAtual * 0.2);
+      novo[ref] = Number(suavizado.toFixed(6));
     } else {
       novo[ref] = rawVal;
     }
